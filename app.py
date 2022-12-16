308c35628c588a90f48d7f00059bac39b120c2b1
Yunwltn <120348555+Yunwltn@users.noreply.github.com>
2022-12-16 15:27:40 +0900

    수정

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from matplotlib import pyplot as plt
@@ -19,6 +20,7 @@ def main() :
     # 업로드한 csv 파일을 데이터프레임으로 읽기
     if file is not None :
         df = pd.read_csv(file)
+        st.success('CSV파일이 업로드 되었습니다')
         st.dataframe(df)
 
         # 비어있는 데이터 확인하고 있으면 제거하기
@@ -32,12 +34,14 @@ def main() :
             df = df[selected_column1] = df[selected_column1].replace(0, np.nan)
             if len(selected_column1) != 0 :
                 st.dataframe(df.isna().sum())
+                st.success('0 데이터가 nan값으로 변경 되었습니다')
 
         if st.checkbox('nan값 확인하기') :
             st.dataframe(df.isna().sum())
             if st.checkbox(':warning: nan값 모두 제거하기 :warning:') :
                 df = df.dropna()
                 st.dataframe(df.isna().sum())
+                st.success('nan값이 모두 제거되었습니다')
 
         # KMeans 클러스터링을 하기 위해 X로 사용할 컬럼을 설정
         st.subheader('')
@@ -55,7 +59,7 @@ def main() :
                 # label 실행
                 encoder = LabelEncoder()
                 X[X_object_column] = pd.DataFrame(encoder.fit_transform( X[X_object_column] ))
-                st.write(':arrow_forward: 문자열 데이터 컬럼 Label Encoding 실행')
+                st.info(':arrow_forward: 문자열 데이터 컬럼 Label Encoding 실행하였습니다')
                 st.dataframe(X)
             elif X[X_object_column].nunique().values > 2:
                 # one-hot 실행
@@ -63,12 +67,18 @@ def main() :
                 X_list_index = X_list.index(X_object_column)
                 ct = ColumnTransformer( [ ( 'encoder', OneHotEncoder() , [X_list_index]  ) ], remainder= 'passthrough' )
                 X = ct.fit_transform(X)
-                st.write(':arrow_forward: 문자열 데이터 컬럼 One-Hot Encoding 실행')
+                st.info(':arrow_forward: 문자열 데이터 컬럼 One-Hot Encoding 실행하였습니다')
                 st.dataframe(X)
+
+            # X값 피처스케일링
+            if st.checkbox('Data Feature Scaling') :  
+                m_scaler_x = MinMaxScaler()
+                X = m_scaler_x.fit_transform(X)
 
         # WCSS를 확인하기 위한 그룹의 갯수 정하기
         st.subheader('')
         st.subheader('클러스터링 갯수 선택 :pushpin:')
+        st.info('WCSS로 그룹 갯수를 추천받아보세요!')
         if st.checkbox('WCSS로 추천받기'):
             max_number = st.slider('WCSS를 확인하기 위한 최대 그룹 갯수 선택', 2, 20, value=10)
             if len(selected_column2) == 0 :
@@ -116,6 +126,7 @@ def main() :
                 csvfile_name = st.text_input('저장할 파일명을 입력하세요 ex)csvfile.csv')
                 if len(csvfile_name) != 0 :
                     df.to_csv(csvfile_name)
+                    st.success(csvfile_name + '파일이 저장되었습니다')
 
 if __name__ == '__main__' :
     main()
